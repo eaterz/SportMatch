1d50308c7f598c608ed3b515dec09ab4f2762f50
eaterz <muraskoruslans@gmail.com>
2025-10-13 15:17:51 +0300

    code fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Sport;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -32,5 +33,30 @@ class DatabaseSeeder extends Seeder
         foreach ($sports as $sport) {
             Sport::create($sport);
         }
+        // Create admin user
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@sportmatch.lv'],
+            [
+                'name' => 'Admin',
+                'lastname' => 'SportMatch',
+                'password' => Hash::make('AdminPassword123!'),
+                'is_admin' => true,
+                'email_verified_at' => now(),
+            ]
+        );
+
+        // Create admin profile
+        $admin->profile()->firstOrCreate([
+            'birth_date' => '1990-01-01',
+            'phone' => '+37112345678',
+            'gender' => 'male',
+            'bio' => 'SportMatch Administrator',
+            'city_id' => 1,
+            'is_verified' => true,
+            'verified_at' => now(),
+            'verification_method' => 'manual'
+        ]);
     }
+
+
 }
